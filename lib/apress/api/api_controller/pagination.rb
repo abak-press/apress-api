45b4beeccf3c3431da3f0e913f818a8037afd60b
Andrey Shmelkov <irratio@gmail.com>
2017-02-21 20:22:06 +0500

    feature(pagination): Link, X-Per-Page, X-Page headers
    В метод #pagination_headers добавлена поддержка заголовков X-Page,
    X-Per-Page и Link (RFC 5988)
    
    https://jira.railsc.ru/browse/RAD-294

--- lib/apress/api/api_controller/pagination.rb
+++ lib/apress/api/api_controller/pagination.rb
@@ -1,8 +1,10 @@
+require 'addressable/uri'
 module Apress
   module Api
     module ApiController
       module Pagination
         class InvalidPaginationError < StandardError; end
+        class LinkHeaderAppendNotImplemented < StandardError; end
 
         extend ActiveSupport::Concern
 
@@ -41,10 +43,64 @@ module Apress
           raise InvalidPaginationError unless (1..max_per_page).include?(@per_page)
         end
 
+        # Public: sets pagination headers
+        #
+        # collection - WillPaginate::Collection
+        #
+        # Returns nothing
         def pagination_headers(collection)
-          headers["X-Total-Count"] = collection.total_entries.to_s
-          headers["X-Total-Pages"] = collection.total_pages.to_s
+          raise LinkHeaderAppendNotImplemented if headers['Link'].present?
+
+          headers.merge!(PaginationHelper.headers(collection, request.url))
         end
+      end
+
+      module PaginationHelper
+        def self.headers(collection, url)
+          result = {}
+
+          result['X-Total-Count'] = collection.total_entries.to_s
+          result['X-Total-Pages'] = collection.total_pages.to_s
+          result['X-Per-Page'] = collection.per_page.to_s
+          result['X-Page'] = collection.current_page.to_s
+
+          numbers = page_numbers(collection)
+          links = header_link_values(url, numbers)
+
+          result['Link'] = links.join(', ') if links.present?
+
+          result
+        end
+
+        def self.header_link_values(url, page_numbers)
+          url = ::Addressable::URI.parse(url)
+          url.query_values ||= {}
+
+          page_numbers.map do |rel, number|
+            url.query_values = url.query_values.merge('page' => number)
+
+            %(<#{url}>; rel="#{rel}")
+          end
+        end
+
+        def self.page_numbers(collection)
+          result = {}
+          return result unless collection.total_pages > 1
+
+          if collection.current_page > 1
+            result[:first] = 1
+            result[:prev] = collection.current_page - 1
+          end
+
+          if collection.current_page < collection.total_pages
+            result[:last] = collection.total_pages
+            result[:next] = collection.current_page + 1
+          end
+
+          result
+        end
+
+        private_class_method :header_link_values, :page_numbers
       end
     end
   end
--- spec/controllers/api_controller/pagination_spec.rb
+++ spec/controllers/api_controller/pagination_spec.rb
@@ -47,22 +47,133 @@ describe Apress::Api::ApiController::Base, type: :controller do
   end
 
   describe "#pagination_headers" do
-    let(:collection) { double(total_entries: 30, total_pages: 10) }
+    let(:current_page) { 1 }
+    let(:collection) do
+      double(
+        total_entries: 30,
+        total_pages: 3,
+        per_page: 10,
+        current_page: current_page
+      )
+    end
 
-    controller do
-      def index
-        render json: {status: "ok"}
+    context 'when there are no other values in Link header' do
+      controller do
+        skip_before_filter :authenticate
+
+        def index
+          pagination_headers(collection)
+          render json: {status: 'ok'}
+        end
+      end
+
+      before do
+        allow(controller).to receive(:collection).and_return(collection)
+        get :index
+      end
+
+      it 'sets correct X-Total-Count header' do
+        expect(response.header['X-Total-Count']).to eq '30'
+      end
+
+      it 'sets correct X-Total-Pages header' do
+        expect(response.header['X-Total-Pages']).to eq '3'
+      end
+
+      it 'sets correct X-Per-Page header' do
+        expect(response.header['X-Per-Page']).to eq '10'
+      end
+
+      context 'when current page is a first page' do
+        it 'sets correct X-Page header' do
+          expect(response.header['X-Page']).to eq '1'
+        end
+
+        it 'does not add pagination links for first and previous pages to Link header' do
+          expect(response.header['Link'].to_s).not_to include('rel="first"', 'rel="prev"')
+        end
+
+        it 'adds pagination links for next and last pages to Link header' do
+          expect(response.header['Link'].to_s.split(', ')).to include(
+            %(<#{request.url}?page=2>; rel="next"),
+            %(<#{request.url}?page=3>; rel="last")
+          )
+        end
+      end
+
+      context 'when current page is somewhere in the middle' do
+        let(:current_page) { 2 }
+
+        it 'sets correct X-Page header' do
+          expect(response.header['X-Page']).to eq '2'
+        end
+
+        it 'adds pagination links for first, previous, next and last pages to Link header' do
+          expect(response.header['Link'].to_s.split(', ')).to include(
+            %(<#{request.url}?page=1>; rel="first"),
+            %(<#{request.url}?page=1>; rel="prev"),
+            %(<#{request.url}?page=3>; rel="next"),
+            %(<#{request.url}?page=3>; rel="last")
+          )
+        end
+      end
+
+      context 'when current page is a last page' do
+        it 'sets correct X-Page header' do
+          expect(response.header['X-Page']).to eq '3'
+        end
+
+        let(:current_page) { 3 }
+
+        it 'does not add pagination links for next and last pages to Link header' do
+          expect(response.header['Link'].to_s).not_to include('rel="next"', 'rel="last"')
+        end
+
+        it 'adds pagination links for first and previous pages to Link header' do
+          expect(response.header['Link'].to_s.split(', ')).to include(
+            %(<#{request.url}?page=1>; rel="first"),
+            %(<#{request.url}?page=2>; rel="prev")
+          )
+        end
+      end
+
+      context 'when there is only one page' do
+        let(:collection) do
+          double(
+            total_entries: 9,
+            total_pages: 1,
+            per_page: 10,
+            current_page: 1
+          )
+        end
+
+        it 'does not add pagination links to Link header' do
+          expect(response.header['Link'].to_s).not_to include('rel="first"', 'rel="prev"', 'rel="last"', 'rel="next"')
+        end
       end
     end
 
-    context "when auth successfull" do
-      before do
-        get :index
-        controller.pagination_headers(collection)
+    context 'when Link header already has some values' do
+      controller do
+        skip_before_filter :authenticate
+
+        def index
+          headers['Link'] = '<http://test.host/help>; rel="help"'
+
+          pagination_headers(collection)
+          render json: {status: 'ok'}
+        end
       end
 
-      it { expect(response.header["X-Total-Count"]).to eq '30' }
-      it { expect(response.header["X-Total-Pages"]).to eq '10' }
+      let(:current_page) { 2 }
+
+      before do
+        allow(controller).to receive(:collection).and_return(collection)
+      end
+
+      it do
+        expect { get :index }.to raise_error(Apress::Api::ApiController::Pagination::LinkHeaderAppendNotImplemented)
+      end
     end
   end
 end
