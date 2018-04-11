ca1c1f2d5e346e69113b9d5fb9d972be1e09bc64
Denis Korobicyn <deniskorobitcin@gmail.com>
2018-04-11 23:41:35 +0500

    fix(callbacks): add exponential backoff
    https://jira.railsc.ru/browse/ORDERS-1484

--- app/jobs/apress/api/fire_callback_job.rb
+++ app/jobs/apress/api/fire_callback_job.rb
@@ -2,10 +2,11 @@ module Apress
   module Api
     class FireCallbackJob
       include Resque::Integration
+      extend Resque::Plugins::ExponentialBackoff
 
       queue :api_callbacks
-      retrys delay: 10, limit: 2
-      @retry_exceptions = [Apress::Api::Callbacks::FireCallbackError]
+
+      @retry_exceptions = {Apress::Api::Callbacks::FireCallbackError => [1, 60, 300, 3600]}
 
       def self.perform(service, event, params)
         callback_class = "#{service}_client/fire_callback".camelize.constantize
